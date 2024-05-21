58bd00c3df858081ebe46aa97d15dbc29a42b5df
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-21 23:04:45 +0700

    feat(main): fixed banner height for mobile display

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -6,8 +6,8 @@ const Banner : React.FC<BannerProps> = ({ section }) => {
     
     return (
         <>
-            <div className="bg-[#0E1F51] flex justify-center items-center h-[280px] w-full">
-                <h2 className="font-bold text-4xl text-[#ff3e54]">{section}</h2>
+            <div className="bg-[#0E1F51] flex justify-center items-center h-[140px] md:h-[280px] w-full">
+                <h2 className="font-bold text-3xl md:text-4xl text-[#ff3e54]">{section}</h2>
             </div>
         </>
     );
