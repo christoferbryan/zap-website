2de2781cf982b66481fe5e0a97bc7339487ea986
Christofer Bryan <christoferbryan45@gmail.com>
2024-05-21 23:03:09 +0700

    feat(main): fixed team members component

--- src/app/team/components/TeamMembers/index.tsx
+++ src/app/team/components/TeamMembers/index.tsx
@@ -11,18 +11,18 @@ const TeamMembers : React.FC = () => {
             <div className="flex flex-col py-10 w-full justify-center items-center gap-4 bg-[#FFFFFF]">
                 <p className="font-bold text-[30px] md:text-[40px] text-[#0E1F51]">Our Team Members</p>
                 <div className="flex flex-col gap-8 md:flex-row justify-center items-center text-black">
-                {MembersData.slice(0,4).map((person, index) => (
-                                            <div key={index} className="flex items-center justify-center w-full h-full">
-                                                <TeamCard {...person} borderRadius={10}/>
-                                            </div>
-                                    ))}
+                    {MembersData.slice(0,4).map((person, index) => (
+                                                <div key={index} className="flex items-center justify-center w-full h-full">
+                                                    <TeamCard {...person} borderRadius={10} />
+                                                </div>
+                    ))}
                 </div>
                 <div className="flex flex-col gap-8 md:flex-row justify-center items-center text-black">
-                {MembersData.slice(4,8).map((person, index) => (
-                                            <div key={index} className="flex items-center justify-center w-full h-full">
-                                                <TeamCard {...person} borderRadius={10} />
-                                            </div>
-                                    ))}
+                    {MembersData.slice(4,8).map((person, index) => (
+                                                <div key={index} className="flex items-center justify-center w-full h-full">
+                                                    <TeamCard {...person} borderRadius={10} />
+                                                </div>
+                    ))}
                 </div>
             </div>
         </>
